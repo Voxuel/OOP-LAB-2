2154aa28684f527a43ab87aee65379ba51ab7e9f
Voxuel <Leo.Fridh@hotmail.com>
2022-10-21 10:37:43 +0200

    Added Snake-Class

---
+++ OOPLAB2/Snake.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLAB2
+{
+    class Snake : Animals
+    {
+        private readonly bool IsPoisonous;
+        public Snake(int _age, string _color, string _gender, bool _isHungry, string _name, bool _isPoisonous)
+        : base(_age, _color, _gender, _isHungry, _name)
+        {
+            this.IsPoisonous = _isPoisonous;
+        }
+        public void SetFree()
+        {
+            if (!IsPoisonous)
+            {
+                Console.WriteLine($"{Name} is set free to play outside the cage.");
+            }
+            else
+            {
+                Console.WriteLine("Ohh I wouldn't do that!");
+            }
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Thhhzzzz");
+        }
+    }
+}
