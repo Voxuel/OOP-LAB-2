c3b0e0730bdbb8b6ceca3468cf1a139540899fa4
Voxuel <Leo.Fridh@hotmail.com>
2022-10-21 10:49:46 +0200

    Added Shiba-SubClass to Dog

--- OOPLAB2/Program.cs
+++ OOPLAB2/Program.cs
@@ -6,8 +6,11 @@ namespace OOPLAB2
     {
         static void Main(string[] args)
         {
-            var cat = new Cat(10,"Black","Male",true,"Joe",false);
+            var cat = new Cat(10,"Grey","Female",false,"Nath",false);
             cat.PrintAnimal();
+            var shiba = new Shiba(10, "Black", "Male", true, "Joe", false);
+            shiba.MakeTrick();
+            shiba.Wash();
         }
     }
 }
---
+++ OOPLAB2/Shiba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPLAB2
+{
+    class Shiba : Dog
+    {
+        private string Trick = "the shiba stands on 2 legs!";
+        public Shiba(int _age, string _color, string _gender, bool _isHungry, string _name, bool _needsWash)
+        : base(_age, _color, _gender, _isHungry, _name, _needsWash)
+        {
+
+        }
+        public void MakeTrick()
+        {
+            Console.WriteLine($"{Name} " + Trick);
+        }
+    }
+}
